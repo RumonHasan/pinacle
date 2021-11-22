087df4321b6961598fbc6099a54708e3f10e7f7c
MHRumon <rumonuniv@gmail.com>
2021-11-22 09:55:21 +0900

    control-error

--- pages/login.js
+++ pages/login.js
@@ -2,15 +2,17 @@ import React, { useContext, useState } from 'react';
 import MainLayout from '../components/MainLayout';
 import { Controller, useForm } from 'react-hook-form';
 import { TaskContext } from '../utils/taskManager';
-import { Box, List, ListItem, Typography } from '@material-ui/core';
+import { Box, List, ListItem, TextField, Typography } from '@material-ui/core';
 import { useSnackbar } from 'notistack';
 import { useRouter } from 'next/dist/client/router';
 import styleObjects from '../utils/styles';
 
 const Login = () => {
     const {state,dispatch} = useContext(TaskContext);
-    const {handleSubmit, control} = useForm();
-    const {enqueueSnackbar} = useSnackbar();
+    const { userInfo } = state;
+    // use form states
+    const {handleSubmit, control, formState:{errors}} = useForm();
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar();
     const router = useRouter();
     // styles
     const {useLoginStyles} = styleObjects();
@@ -18,6 +20,7 @@ const Login = () => {
 
     // form submit handler
     const submitHandler = async ()=>{
+        closeSnackbar();
         try{
 
         }catch(err){
@@ -33,7 +36,40 @@ const Login = () => {
                     <Typography variant='h4'>Login Page</Typography>
                     <List>
                         <ListItem>
-                            <Controller/>
+                            <Controller
+                                name='email'
+                                control={control}
+                                defaultValue=''
+                                rules={{
+                                    required:true,
+                                    pattern:  /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                                }}
+                                render={({field})=>(
+                                    <TextField
+                                        variant='outlined'
+                                        fullWidth
+                                        id='email'
+                                        label='Email'
+                                        inputProps={{type:'email'}}
+                                        error={Boolean(errors.email)}
+                                        helperText={
+                                            errors.email
+                                            ? errors.email.type === 'pattern'
+                                                ? 'Email is not valid'
+                                                : 'Email is required'
+                                            : ''
+                                        }
+                                        {...field}
+                                    >
+                                    </TextField>
+                                )}
+                            ></Controller>
+                        </ListItem>
+
+                        <ListItem>
+                            <Controller
+
+                            />
                         </ListItem>
                     </List>
                 </form>
